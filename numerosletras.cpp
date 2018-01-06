f7586660b3ce2579e73ca890a7999fe42a8d5f78
alanalcaraz <31902909+alanalcaraz@users.noreply.github.com>
2018-01-06 17:00:35 -0600

    Add files via upload
    Programa que lee 5 números (de 1 dígito) y los muestra en letra separados por coma.

---
+++ numerosletras.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int n1,n2,n3,n4,n5;
+	cout<<"Ingrese 5 numeros"<<endl;
+	cout<<"Primer numero: ";
+	cin>>n1;
+	cout<<"Segundo numero: ";
+	cin>>n2;
+	cout<<"Tercer numero: ";
+	cin>>n3;
+	cout<<"Cuarto numero: ";
+	cin>>n4;
+	cout<<"Quinto numero: ";
+	cin>>n5;
+	
+	cout<<"Numeros: "<<n1<<n2<<n3<<n4<<n5<<endl;
+	
+	switch (n1)
+	{
+		case 1:cout<<"Uno"<<","; break;
+		case 2:cout<<"Dos"<<","; break;
+		case 3:cout<<"Tres"<<","; break;
+		case 4:cout<<"Cuatro"<<","; break;
+		case 5:cout<<"Cinco"<<","; break;
+		case 6:cout<<"Seis"<<","; break;
+		case 7:cout<<"Siete"<<","; break;
+		case 8:cout<<"Ocho"<<","; break;
+		case 9:cout<<"Nueve"<<","; break;
+	}
+	switch (n2)
+	{
+		case 1:cout<<"Uno"<<","; break;
+		case 2:cout<<"Dos"<<","; break;
+		case 3:cout<<"Tres"<<","; break;
+		case 4:cout<<"Cuatro"<<","; break;
+		case 5:cout<<"Cinco"<<","; break;
+		case 6:cout<<"Seis"<<","; break;
+		case 7:cout<<"Siete"<<","; break;
+		case 8:cout<<"Ocho"<<","; break;
+		case 9:cout<<"Nueve"<<","; break;
+	}
+	switch (n3)
+	{
+		case 1:cout<<"Uno"<<","; break;
+		case 2:cout<<"Dos"<<","; break;
+		case 3:cout<<"Tres"<<","; break;
+		case 4:cout<<"Cuatro"<<","; break;
+		case 5:cout<<"Cinco"<<","; break;
+		case 6:cout<<"Seis"<<","; break;
+		case 7:cout<<"Siete"<<","; break;
+		case 8:cout<<"Ocho"<<","; break;
+		case 9:cout<<"Nueve"<<","; break;
+	}
+	switch (n4)
+	{
+		case 1:cout<<"Uno"<<","; break;
+		case 2:cout<<"Dos"<<","; break;
+		case 3:cout<<"Tres"<<","; break;
+		case 4:cout<<"Cuatro"<<","; break;
+		case 5:cout<<"Cinco"<<","; break;
+		case 6:cout<<"Seis"<<","; break;
+		case 7:cout<<"Siete"<<","; break;
+		case 8:cout<<"Ocho"<<","; break;
+		case 9:cout<<"Nueve"<<","; break;
+	}
+	switch (n5)
+	{
+		case 1:cout<<"Uno"<<","; break;
+		case 2:cout<<"Dos"<<","; break;
+		case 3:cout<<"Tres"<<","; break;
+		case 4:cout<<"Cuatro"<<","; break;
+		case 5:cout<<"Cinco"<<","; break;
+		case 6:cout<<"Seis"<<","; break;
+		case 7:cout<<"Siete"<<","; break;
+		case 8:cout<<"Ocho"<<","; break;
+		case 9:cout<<"Nueve"<<","; break;
+	}
+		
+
+	
+}

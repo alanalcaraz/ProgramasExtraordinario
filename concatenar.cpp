a727fc22be672e89cc33e3c46e19eddc2081e796
alanalcaraz <31902909+alanalcaraz@users.noreply.github.com>
2018-01-08 07:38:21 -0600

    Add files via upload
    Programa con función que concatena 2 cadenas sin strcat.

---
+++ concatenar.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+
+void concatenar(char cad1[], char cad2[]);
+
+int main()
+{
+	string a = "Probando";
+    string b = "Concatenacion";
+	string c = b+a;
+	concatenar;
+	
+	cout<<c;
+}
+
+void concatenar(char cad1[], char cad2[])
+{
+	string a, b;
+    string c = a + " "+b;
+    cout<<c<<endl;
+}
